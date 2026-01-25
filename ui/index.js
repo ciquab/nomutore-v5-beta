9fd92dfc2b9f247f3bc12720b6809ae62fc48365
ciquab <ouioa8.15pc@gmail.com>
2026-01-25 23:30:35 +0900

    Update index.js

--- ui/index.js
+++ ui/index.js
@@ -2,7 +2,7 @@ import { Calc } from '../logic.js';
 import { Store, db } from '../store.js';
 import { Service } from '../service.js';
 import { APP, CHECK_SCHEMA } from '../constants.js';
-import { DOM, toggleModal, showConfetti, showMessage, applyTheme, toggleDryDay, initTheme, Feedback } from './dom.js';
+import { DOM, toggleModal, showConfetti, showToastAnimation, showMessage, applyTheme, toggleDryDay, initTheme, Feedback } from './dom.js';
 import { StateManager } from './state.js';
 
 import { renderBeerTank } from './beerTank.js';
@@ -477,9 +477,10 @@ export const UI = {
     openTimer: openTimer,
     closeTimer: closeTimer,
     refreshUI: refreshUI,
-    
+
     showConfetti: showConfetti,
     showMessage: showMessage,
+    showToastAnimation: showToastAnimation,
     
     openActionMenu: openActionMenu,
     handleActionSelect: handleActionSelect,
@@ -504,6 +505,7 @@ export {
     StateManager,
     toggleModal
 };
+
 
 
 

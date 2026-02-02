986f2635ecebc76a583c1b35e9e0708ecdfdcf00
ciquab <ouioa8.15pc@gmail.com>
2026-02-02 13:08:53 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nomutore-v5.0';
+const CACHE_NAME = 'nomutore-v5.0.1';
 const APP_SHELL = [
     './',
     './index.html',
@@ -36,7 +36,6 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
     );
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {

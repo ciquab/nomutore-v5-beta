d4228e0db6482cb58d1426ac42752f3c883265bb
ciquab <ouioa8.15pc@gmail.com>
2026-02-03 07:32:44 +0900

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nomutore-v5.0.7';
+const CACHE_NAME = 'nomutore-v0.9.0';
 const APP_SHELL = [
     './',
     './index.html',
@@ -109,6 +109,7 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
+
 
 
 

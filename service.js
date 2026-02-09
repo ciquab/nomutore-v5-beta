677b084b8ceb9080e376b3a919c889e63c27d8fd
ciquab <ouioa8.15pc@gmail.com>
2026-02-09 23:13:55 +0900

    Update service.js

--- service.js
+++ service.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { db, Store } from './store.js';
+import { logService } from './logService.js';
 import { Calc, getVirtualDate } from './logic.js';
 import { APP, EXERCISE, STYLE_SPECS } from './constants.js';
 import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@1.11.10/+esm';
@@ -889,6 +890,7 @@ saveDailyCheck: async (formData) => {
     };
 },
 };
+
 
 
 

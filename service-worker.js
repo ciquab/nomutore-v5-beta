923708766981095d7fb7fd537132e6c1ef85e1cb
ciquab <ouioa8.15pc@gmail.com>
2026-02-13 10:31:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // @ts-check
-const CACHE_NAME = 'nomutore-v0.1.8';
+const CACHE_NAME = 'nomutore-v0.1.9';
 const APP_SHELL = [
     './',
     './index.html',
@@ -123,6 +123,7 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
+
 
 
 
